7f82c3c0d816ab23b10f03c5a35d9fb66dac3e95
vagrant <vagrant@lumen>
2018-02-17 23:50:05 +0000

    Adding app configuration files, routes file (without any route yet), the phpmig bin folder and the phpmig config file adapted to Eloquent

---
+++ bootstrap/config.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Capsule\Manager as Capsule;
+
+$app['db'] = $app->share(function() use($app){
+
+    //Set up Fluent Query Builder..
+    $capsule = new Capsule;
+    $capsule->addConnection([
+        'driver'    => 'mysql',
+        'host'      => '127.0.0.1',
+        'database'  => 'silex',
+        'username'  => 'homestead',
+        'password'  => 'secret',
+        'charset'   => 'utf8',
+        'collation' => 'utf8_general_ci',
+    ]);
+    return $capsule;
+
+});
+
+//These two have to be outside closure
+// Make the Capsule instance available globally via static methods...
+$app['db']->setAsGlobal();
+// Setup the Eloquent ORM...
+$app['db']->bootEloquent();
+
+/*
+$capsule = new Capsule();
+
+$capsule->addConnection([
+    'driver'    => 'mysql',
+    'host'      => '127.0.0.1',
+    'database'  => 'silex',
+    'username'  => 'homestead',
+    'password'  => 'secret',
+    'charset'   => 'utf8',
+    'collation' => 'utf8_general_ci',
+]);
+
+$capsule->bootEloquent();*/
