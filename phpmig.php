46252ea290437fc04d1ae7264d560b67dba49a58
Mariano Gomez <marianogomez@gmail.com>
2018-02-17 16:44:25 -0300

    Migration config file updated to use Eloquent ORM

--- phpmig.php
+++ phpmig.php
@@ -1,18 +1,35 @@
 <?php
 
-use \Phpmig\Adapter;
+use Phpmig\Adapter;
+use Pimple\Container;
+use Illuminate\Database\Capsule\Manager as Capsule;
 
-$container = new ArrayObject();
+$container = new Container();
 
-// replace this with a better Phpmig\Adapter\AdapterInterface
-$container['phpmig.adapter'] = new Adapter\File\Flat(__DIR__ . DIRECTORY_SEPARATOR . 'migrations/.migrations.log');
+$container['config'] = [
+    'driver'    => 'mysql',
+    'host'      => '127.0.0.1',
+    'database'  => 'silex',
+    'username'  => 'homestead',
+    'password'  => 'secret',
+    'charset'   => 'utf8',
+    'collation' => 'utf8_general_ci',
+];
+
+$container['db'] = function ($c) {
+    $capsule = new Capsule();
+    $capsule->addConnection($c['config']);
+    $capsule->setAsGlobal();
+    $capsule->bootEloquent();
+
+    return $capsule;
+};
+
+
+$container['phpmig.adapter'] = function ($c) {
+    return new Adapter\Illuminate\Database($c['db'], 'migrations');
+};
 
 $container['phpmig.migrations_path'] = __DIR__ . DIRECTORY_SEPARATOR . 'migrations';
-
-// You can also provide an array of migration files
-// $container['phpmig.migrations'] = array_merge(
-//     glob('migrations_1/*.php'),
-//     glob('migrations_2/*.php')
-// );
 
 return $container;
