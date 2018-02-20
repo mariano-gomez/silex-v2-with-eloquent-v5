7574cb5adf9415c9fef30e630582e5365a307bcb
Mariano Gomez <marianogomez@gmail.com>
2018-02-20 00:39:58 -0300

    removing a premature return command on the eloquent service configuration

--- bootstrap/config.php
+++ bootstrap/config.php
@@ -13,27 +13,12 @@ $capsule->addConnection([
     'charset'   => 'utf8',
     'collation' => 'utf8_general_ci',
 ]);
-return $capsule;
-
-$app['db'] = $capsule;
 
 //These two have to be outside closure
 // Make the Capsule instance available globally via static methods...
-$app['db']->setAsGlobal();
+$capsule->setAsGlobal();
 // Setup the Eloquent ORM...
-$app['db']->bootEloquent();
+$capsule->bootEloquent();
 
-/*
-$capsule = new Capsule();
+$app['db'] = $capsule;
 
-$capsule->addConnection([
-    'driver'    => 'mysql',
-    'host'      => '127.0.0.1',
-    'database'  => 'silex',
-    'username'  => 'homestead',
-    'password'  => 'secret',
-    'charset'   => 'utf8',
-    'collation' => 'utf8_general_ci',
-]);
-
-$capsule->bootEloquent();*/
